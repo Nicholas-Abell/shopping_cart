d228d28bd8838fa097c5c0b1274d4fda0b0785ce
Nick Abell <darkforceyoda@gmail.com>
2023-02-22 11:42:47 -0600

    created ShoppingCartContext logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
+import { ShoppingCartProvider } from './context/shoppingCartContext'
 
 import Home from './pages/Home'
 import Store from './pages/Store'
@@ -9,14 +10,16 @@ import Navbar from './components/Navbar'
 function App() {
 
   return (
-    <Container className='mb-4'>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/store' element={<Store />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
-    </Container>
+    <ShoppingCartProvider>
+      <Container className='mb-4'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/store' element={<Store />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
   )
 }
 
